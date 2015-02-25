2ef27021afa6f3563764551edcd3312f3f11529e
Phil B <philb@seepies.net>
2015-02-24 22:37:25 -0500

    Added How To Enter/Login To Vote dynamic margin

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,13 +1,17 @@
         <div class="top_menu">
-            <div class="how_to_enter">
-                <a href="/how-to-enter">
-                    <img src="/images/how_to_enter.png" alt="How to enter" border="0" />
-                </a>
-            </div>
-            <div class="login_to_vote">
-                <img src="/images/login_to_vote.png" alt="Login to vote" border="0" />
+            <div class="top_menu_container">
+                <div class="login_to_vote">
+                    <img src="/images/login_to_vote.png" alt="Login to vote" border="0" />
+                </div>
+                <div class="how_to_enter">
+                    <a href="/how-to-enter">
+                        <img src="/images/how_to_enter.png" alt="How to enter" border="0" />
+                    </a>
+                </div>
+
             </div>
         </div>
+        <div class="clear"></div>
         <div class="menu_container">
             <a class="menu_link <?php echo $nav['dactive'] ? 'active' : ''; ?>" href="/">Designs</a>
             <a class="menu_link <?php echo $nav['aactive'] ? 'active' : ''; ?>" href="/about">About the Competition</a>
