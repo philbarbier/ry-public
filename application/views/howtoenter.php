14797e6d9ae2c9a2de53d8271b0e09e1d08fe4cc
Phil B <philb@seepies.net>
2015-03-02 19:59:56 -0500

    Rules/Regs file change, date change in how to enter page

--- application/views/howtoenter.php
+++ application/views/howtoenter.php
@@ -5,10 +5,7 @@ HOW TO ENTER:
 PROFILE: An invitational student art competition
 </p>
 <p>
-WHO: Undergraduate level illustration and design arts students are eligible from the following schools:<br /><br />
-Vancouver College of Art &amp; Design<br />
-Nova Scotia College of Art &amp; Design<br />
-Emily Carr University of Art &amp; Design<br />
+WHO: Undergraduate level illustration and design arts students are eligible.<br /><br />
 </p>
 <p>
 HOW: Complete the RYVITA Submission Form, and include the artist statement, decription of submission and a PDF of the final design.<br />
@@ -24,11 +21,11 @@ E-mail: <a href="mailto:gcharron@influencemarketing.ca">gcharron@influencemarket
 Forms and digital imagery may be submitted by email only.
 </p>
 <p>
-WHEN: Submission deadline is February 27, 2015
+WHEN: Submission deadline is March 15th, 2015
 </p>
 
 <h2>SUPPORTING DOCUMENTS (DOWNLOADS):</h2>
-<a href="/images/2015_Ryvita_Tin_Rules_Regs_Jan 30_15.pdf" target="_blank">Rules &amp; Regulations (PDF)</a><br />
+<a href="/images/2015_Ryvita_Tin_Rules_Regs_Mar_2_15.pdf" target="_blank">Rules &amp; Regulations (PDF)</a><br />
 <a href="/images/RYVITA_CANADA_TIN_DESIGN_SUBMISSION_FORM_Jan_30_15.pdf" target="_blank">Submission Document (PDF)</a><br />
 <a href="/images/Ryvita_MultiGrain_Tin_Design_TOP_HRZ.pdf" target="_blank">Ryvita Tin Design Top (PDF)</a><br />
 <a href="/images/Ryvita_MultiGrain_Tin_Design_HRZ.pdf" target="_blank">Ryvita Tin Design (PDF)</a><br />
